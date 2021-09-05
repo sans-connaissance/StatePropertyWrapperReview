ca700f18d659caf98bbf204ddd31fec3cae4e522
sans-connaissance <davemalicke@gmail.com>
2021-09-05 11:19:59 -0400

    Initial Commit

---
+++ StatePropertyWrapperReview/StatePropertyWrapperReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatePropertyWrapperReviewApp.swift
+//  StatePropertyWrapperReview
+//
+//  Created by David Malicke on 9/5/21.
+//
+
+import SwiftUI
+
+@main
+struct StatePropertyWrapperReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
